f9e0909ed947f7c7f39381922531d4bd71073b90
StarlexAtlas <201800301032@mail.sdu.edu.cn>
2018-11-29 14:21:47 +0800

    第一次修改，在增加提取样本这个功能时注意到了StringBuilder的使用方法并重写了文件读取部分使之更加完整健壮并兼容了提取样本功能。 -重构了文件读取部分 -增加了样本提取功能

--- src/com/exercise/T03StringProc.java
+++ src/com/exercise/T03StringProc.java
@@ -14,21 +14,32 @@ public class T03StringProc {
     public static void main(String[] args) throws FileNotFoundException {
         String filename = "text01.txt";
         Scanner sc = new Scanner(new File(filename));
+        Random random = new Random();
+        StringBuilder line = new StringBuilder();
+        String fileContent;
+        final int MAX = 100;
         double sNum = 0;
         double caseNum = 0;
-        while (true) {
-            if (sc.hasNext()) {
-                String line = sc.nextLine();
-                for (int i = 0; i < line.length(); i++) {
-                    if (line.charAt(i) == 's') {
-                        sNum++;
-                    }
-                    caseNum++;
-                }
-            } else {
-                break;
-            }
+        double sSimple = 0;
+        double caseSimple = 0;
+        if (sc.hasNext()) {
+            line.append(sc.nextLine());
         }
-        System.out.println("percentage of 's' is " + sNum / caseNum);
+        fileContent = line.toString();
+        for(int i = 0;i < fileContent.length();i++){
+            if(fileContent.charAt(i) == 's'){
+                sNum++;
+            }
+            caseNum++;
+        }
+        for(int i = 0;i < MAX;i++){
+            int rd = random.nextInt(fileContent.length());
+            if(fileContent.charAt(rd) == 's'){
+                sSimple++;
+            }
+            caseSimple++;
+        }
+        System.out.println("percentage of 's' is " + (sNum / caseNum)*100 +"%");
+        System.out.println("percentage of 's' in simple is "+ (sSimple/caseSimple)*100 +"%");
     }
 }
